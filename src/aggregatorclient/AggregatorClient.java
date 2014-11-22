a058e7a361a03b9255a9aef118b89eef3e278c1b
Thufir <hawat.thufir@gmail.com>
2014-11-22 02:39:23 -0800

    increment

--- src/aggregatorclient/AggregatorClient.java
+++ src/aggregatorclient/AggregatorClient.java
@@ -1,5 +1,6 @@
 package aggregatorclient;
 
+import dur.ejb.AnswerSessionBeanRemote;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.Context;
@@ -12,16 +13,18 @@ public class AggregatorClient {
 
     public static void main(String[] args) {
         try {
-            new AggregatorClient().foo();
+            new AggregatorClient().remoteEJB();
         } catch (NamingException ex) {
             Logger.getLogger(AggregatorClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    private void foo() throws NamingException {
+    private void remoteEJB() throws NamingException {
         Context ctx = new InitialContext();
         log.fine(ctx.getEnvironment().toString());
         Object obj = ctx.lookup("ejb.EJBRemoteInterface");
+        AnswerSessionBeanRemote asbr = (AnswerSessionBeanRemote) obj;
+        log.info("answer\t" + asbr.lifeTheUniverseAndEverything());
     }
 
 }
